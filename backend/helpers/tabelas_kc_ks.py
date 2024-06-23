d72c1a7292e6ed44fbf45870d65de422e35d3e6a
alanpallu <alanpallu@gmail.com>
2024-06-23 15:16:18 -0300

    Ajustando deploy backend

--- backend/helpers/tabelas_kc_ks.py
+++ backend/helpers/tabelas_kc_ks.py
@@ -1,20 +1,18 @@
 import pandas as pd
-import psycopg2
-from sqlalchemy import create_engine
 
 
-def create_table_from_excel_file(excel_file, table_name, database_name
-                                 , server_name, port, user_name, password):
-    engine = create_engine(f'postgresql://'
-    f'{user_name}:{password}@{server_name}:{port}/{database_name}')
+# def create_table_from_excel_file(excel_file, table_name, database_name
+#                                  , server_name, port, user_name, password):
+#     engine = create_engine(f'postgresql://'
+#     f'{user_name}:{password}@{server_name}:{port}/{database_name}')
 
-    conn = engine.connect()
+#     conn = engine.connect()
 
-    # read Excel file
-    df = pd.read_excel(excel_file)
+#     # read Excel file
+#     df = pd.read_excel(excel_file)
 
-    # create a table in the database
-    df.to_sql(table_name, conn, if_exists='replace', index=True)
+#     # create a table in the database
+#     df.to_sql(table_name, conn, if_exists='replace', index=True)
 
 
 def select_all():
